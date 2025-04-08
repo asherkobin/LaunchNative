6679b3196d075740a8e735f92dfa681649bf119f
Asher Kobin <asherkobin@gmail.com>
2025-04-08 00:53:00 -0700

    added test cases to prepare for unicode

--- ARMLib/strlen.c
+++ ARMLib/strlen.c
@@ -5,12 +5,31 @@ extern int arm_strlen(const char* str);
 
 int main()
 {
-	char* str = NULL;// "";
-	int cch = arm_strlen(str);
+	char *str_list[] =
+	{
+		"DUMMY",
+		"asdf",
+		".",
+		"The silver spot price today is $30.38 USD.",
+		NULL,
+		"Charles Leclerc is from Monaco",
+		"",
+		"pi = 3.14",
+		"The gold spot price today is $3,013.80 USD.",
+		"asdfasdfsadfsdaf"
+	};
 
-	char buffer[256];
-	StringCchPrintfA(buffer, 256, "arm_strlen: \"%s\" = %d\n", str, cch);
-	OutputDebugStringA(buffer);
+	int num_strs = sizeof(str_list) / 8;
 
-	return cch;
+	for (int i = 0; i <= num_strs - 1; i++)
+	{
+		char* str = str_list[i];
+		int cch = arm_strlen(str);
+
+		char buffer[256];
+		StringCchPrintfA(buffer, 256, "arm_strlen: \"%s\" = %d\n", str, cch);
+		OutputDebugStringA(buffer);
+	}
+
+	return 1;
 }
--- ARMLib/strlen_extern.asm
+++ ARMLib/strlen_extern.asm
@@ -2,22 +2,25 @@
 	EXPORT	arm_strlen
 
 arm_strlen
+		cbz         x0,null_string			; do nothing for a NULL sring
 
-	cbz         x0,null_string			; do nothing for a NULL sring
+		ldrsb		x8,[x0]					; load first character into x8
+		mov			x9,x0					; save x0 into x9
+		cbz			x8,zero_length_string	; handle if string is "\0"
 
-	ldrsb		x8,[x0]					; load first character into x8
-	mov			x9,x0					; save x0 into x9
-	cbz			x8,zero_length_string	; handle if string is "\0"
 non_zero_character
-	ldrsb		x8,[x9,#1]!				; load next character into x8
-	cbnz		x8,non_zero_character	; compare and branch if x8 is > 0x00
-	sub			x8,x9,x0				; this will give the count of characters in x8
-	mov			x0,x8					; x1 is returned to the calller
+		ldrsb		x8,[x9,#1]!				; load next character into x8
+		cbnz		x8,non_zero_character	; compare and branch if x8 is > 0x00
+		sub			x8,x9,x0				; this will give the count of characters in x8
+		mov			x0,x8					; x0 is returned to the calller
+		ret
 
 zero_length_string
-	mov			x0,#0
+		mov			x0,#0
+		ret
+
 null_string
-	mov			x0,#0
-	ret									; mission complee!
+		mov			x0,#0
+		ret									; mission complee!
 
 END
